e5833815f388281698f8c32a9554d77f5cb30ef6
Lizbeth <Lizbeth@Lizbeths-MacBook-Pro.local>
2017-02-21 14:28:36 -0600

    adding comments

--- scripts/script.js
+++ scripts/script.js
@@ -14,18 +14,38 @@ var billsPromise = $.getJSON(url)//sends the request
 //write a function containing the code that depends on the API response
 //give that function to the promise using promise.then(), so the promise can run it later
 
-var makeBillHTML = function(arrayObj){//takes in a single object of the array and converts the object into html format
+var makeBillHTML = function(singleBillObj){//takes in a single object of the array and converts the object into html format
 	var getHTML = ''
-	//getHTML += '<div class="bill">' + 
-}
+	
+	getHTML += '<div class="bill">'           //we create the div  
+	getHTML +=  singleBillObj.official_title  //we call the title method in the singleBill object
+	getHTML +=  '</div>'	
+	return getHTML
 
-function handleBillsResponse(billsObj) {//takes object from API response and processes data at each iteration
+	}
+
+
+function handleBillsResponse(billsObj) { //takes object from API response and processes data at each iteration
 	console.log(billsObj)
 	var allBillsHTML = ''
+
+	//by using document.querySelector we select that space 
+	var containerNode = document.querySelector('.container')
 	var billsArray = billsObj.results
-	for(var i = 0; i < billsArray.length; i++) {
-		allBillsHTML += makeBillHTML(billsArray[i])
-	}
+
+    //we iterate into each object of the array
+		for(var i = 0; i < billsArray.length; i++) {
+			allBillsHTML += makeBillHTML(billsArray[i]) //allBillsHTML become the return value of the functionmakeBillHTML
+			
+		}
+
+    console.log(allBillsHTML)
+
+	containerNode.innerHTML = allBillsHTML // we display the title into the page
 }
 
 billsPromise.then(handleBillsResponse) // take this function and hang onto it(handleBillsResponse)
+
+
+
+
